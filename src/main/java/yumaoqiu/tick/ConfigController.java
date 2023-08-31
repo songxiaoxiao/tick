3dfc629484e29e8a887d5b9bad0ec5405a397191
winter.song <2261617274@qq.com>
2023-08-31 11:36:43 +0800

    aaa

--- src/main/java/yumaoqiu/tick/ConfigController.java
+++ src/main/java/yumaoqiu/tick/ConfigController.java
@@ -1,5 +1,6 @@
 package yumaoqiu.tick;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.*;
  * @className: ConfigController
  * @date 2023/8/30 11:42 AM
  */
+@Slf4j
 @RestController
 public class ConfigController {
     @RequestMapping("/config")
     public String config(@RequestParam("key") String key, @RequestParam("cdstring") String cdstring) {
+        log.info("key: {}, cdstring: {}", key, cdstring);
         ScheduledTask.setWxKey(key);
         return "OK";
     }
