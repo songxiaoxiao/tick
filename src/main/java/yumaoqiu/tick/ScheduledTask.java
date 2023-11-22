eb0df1b736a9f7f672aa8ce01ee97b2ff2808f55
winter.song <2261617274@qq.com>
2023-11-22 12:46:21 +0800

    测试提前一秒延迟950ms 是否恰当大都好处接受请求

--- src/main/java/yumaoqiu/tick/ScheduledTask.java
+++ src/main/java/yumaoqiu/tick/ScheduledTask.java
@@ -96,7 +96,7 @@ public class ScheduledTask {
          * @date: 2023/8/30 11:27 AM
          * @return
          */
-    @Scheduled(cron = "59 59 05 * * 4", zone = "Asia/Shanghai")
+    @Scheduled(cron = "00 00 06 * * 5", zone = "Asia/Shanghai")
     public void task() {
         log.info("========定时抢羽毛球场地 begin==========");
         //  获取当前日期+2天
@@ -132,8 +132,13 @@ public class ScheduledTask {
 
 
 
-    @Scheduled(cron = "59 59 05 * * 3", zone = "Asia/Shanghai")
-    public void test() {
+    @Scheduled(cron = "59 59 05 * * 4", zone = "Asia/Shanghai")
+    public void test(){
+        try{
+            Thread.sleep(950);
+        }catch (Exception e){
+            log.info("sleep error");
+        }
         log.info("========定时抢羽毛球场地 begin==========");
         //  获取当前日期+2天
         String date2 = getDate2();
@@ -164,7 +169,7 @@ public class ScheduledTask {
         log.info("========定时抢羽毛球场地 end==========");
     }
 
-    @Scheduled(cron = "00 50 05 * * 3", zone = "Asia/Shanghai")
+    @Scheduled(cron = "00 50 05 * * 4", zone = "Asia/Shanghai")
     public void yure() {
         log.info("========定时抢羽毛球场地 begin==========");
         //  获取当前日期+2天
