88b00f7f159e0d9a2103ebd21a3ffe12308b3cb7
winter.song <2261617274@qq.com>
2023-11-23 10:40:08 +0800

    05 59 59

--- src/main/java/yumaoqiu/tick/ScheduledTask.java
+++ src/main/java/yumaoqiu/tick/ScheduledTask.java
@@ -132,7 +132,7 @@ public class ScheduledTask {
 
 
 
-    @Scheduled(cron = "59 59 05 * * 4", zone = "Asia/Shanghai")
+    @Scheduled(cron = "59 59 05 * * 5", zone = "Asia/Shanghai")
     public void test(){
         try{
             Thread.sleep(950);
@@ -169,7 +169,7 @@ public class ScheduledTask {
         log.info("========定时抢羽毛球场地 end==========");
     }
 
-    @Scheduled(cron = "00 50 05 * * 4", zone = "Asia/Shanghai")
+    @Scheduled(cron = "00 50 05 * * 5", zone = "Asia/Shanghai")
     public void yure() {
         log.info("========定时抢羽毛球场地 begin==========");
         //  获取当前日期+2天
